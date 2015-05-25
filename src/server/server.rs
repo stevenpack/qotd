5453e2a0514bc21b0b5812e5788eed60e7cb569d
stevenpack <steven_john_pack@yahoo.com.au>
2015-05-24 21:56:19 -0700

    tcp working. udp in progress

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,7 +1,10 @@
 extern crate mio;
 extern crate rand;
 
+use mio::{EventLoop,Token,TryWrite};
 use mio::tcp::*;
+use mio::udp::*;
+use mio::buf::*;
 use rand::{thread_rng, Rng};
 use std::fs::File;
 use std::io::*;
@@ -40,11 +43,11 @@ impl QuoteProviderImpl for QuoteProvider {
     match result {
         Ok(file) => {
             let reader = BufReader::new(file);
-            for line in reader.lines {
+            for line in reader.lines() {
                 match line {
                     Ok (line_str) => {
                         if line_str.len() > 0 {
-                            quotes.push(line_str);
+                            quotes.push(line_str + "\r\n");
                         } else {
                             println!("Skipped blank line in file.");
                         }
@@ -61,11 +64,13 @@ impl QuoteProviderImpl for QuoteProvider {
   }
 }
 
-
-const SERVER: mio::Token = mio::Token(0);
+const TCP_SERVER: mio::Token = mio::Token(0);
+const UDP_SERVER: mio::Token = mio::Token(1);
 
 struct Pong {
-    server: TcpListener,
+    tcp_server: TcpListener,
+    udp_server: UdpSocket,
+    quote_provider: QuoteProvider
 }
 
 impl mio::Handler for Pong {
@@ -74,12 +79,13 @@ impl mio::Handler for Pong {
 
     fn readable(&mut self, event_loop: &mut mio::EventLoop<Pong>, token: mio::Token, hint: mio::ReadHint) {
         match token {
-            SERVER => {
-                println!("the server socket is ready to accept a connection");
-                match self.server.accept() {
-                    Ok(Some(connection)) => {
-                        println!("accepted a socket, exiting program");
-                        event_loop.shutdown();
+            TCP_SERVER => {
+                println!("the server socket is ready to accept a TCP connection");
+                match self.tcp_server.accept() {
+                    Ok(Some(mut connection)) => {
+                        println!("accepted a socket {:?}", connection);
+                        let quote = self.quote_provider.get_random_quote();
+                        connection.write_slice(quote.as_bytes());
                     }
                     Ok(None) => {
                         println!("the server socket wasn't actually ready");
@@ -90,8 +96,28 @@ impl mio::Handler for Pong {
                     }
                 }
             }
+            UDP_SERVER => {
+                println!("the server socket is ready to accept a UDP connection");
+                //let mut buf = [0, ..10];
+                match self.udp_server.recv_from(buf) {
+                    Ok((amt, src)) => {
+                        // Send a reply to the socket we received data from
+                        //let buf = buf.mut_slice_to(amt);
+                        //buf.reverse();
+                        //self.udp_server.send_to(buf, src);
+                    }
+                    Err(e) => println!("couldn't receive a datagram: {}", e)
+                }
+            }
             _ => panic!("Received unknown token"),
         }
+    }
+
+    fn writable(&mut self, event_loop: &mut EventLoop<Pong>, token: Token) {
+        match token {
+            TCP_SERVER => println!("received writable for token 0"),
+            _ => panic!("Unexpected token")
+        };
     }
 }
 
@@ -101,11 +127,22 @@ fn main() {
     println!("Sample quote: {:?}", quote_provider.get_random_quote());
 
     let address = "0.0.0.0:6567".parse().unwrap();
-    let server = TcpListener::bind(&address).unwrap();
+    let tcp_server = TcpListener::bind(&address).unwrap();
+    let udp_server = UdpSocket::v4().unwrap();
+    udp_server.bind(&address);
 
     let mut event_loop = mio::EventLoop::new().unwrap();
-    let _ = event_loop.register(&server, SERVER);
+    let _ = event_loop.register(&tcp_server, TCP_SERVER);
 
     println!("running pingpong server");
-    let _ = event_loop.run(&mut Pong { server: server });
+    let _ = event_loop.run(&mut Pong 
+    {
+        tcp_server: tcp_server,
+        udp_server: udp_server,
+        quote_provider: quote_provider        
+    });
+
+    drop(udp_server); // close the socket
+    drop(tcp_server);
+
 }
